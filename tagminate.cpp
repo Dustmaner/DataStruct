97a29dee90d591e9e7f81b4f1987e1d5fcad0266
Dustmaner <erick.soy@hotmail.com>
2016-03-25 02:10:37 -0500

    Update tagminate.cpp

--- tagminate.cpp
+++ tagminate.cpp
@@ -9,14 +9,77 @@
 #include <algorithm>
 #include <cctype>
 #include <stdlib.h>
+#include <stack>
 
 using namespace std;
 
+struct node {
+	char data;
+	node *next;
+	node *prev;
+};
+
+struct node *newNode(char data)// add new node with int data input
+{
+	struct node *temp = new node;
+	temp->data = data;
+	temp->next = NULL;
+	temp->prev = NULL;
+	return temp;
+};
+
+void addatend(struct node *start, char num)
+{
+	if (start == NULL)
+	{
+		start = new node;
+		start->data = num;
+		start->next = NULL;
+		start->prev = NULL;
+		return;
+	}
+
+	struct node *temp, *p;
+
+	temp = newNode(num);
+	temp->data = num;
+	p = start;
+	while (p->next != NULL)
+	{
+		p = p->next;
+	}
+	//cout << p->data << endl;
+	//cout << temp->data << endl;
+
+
+	p->next = temp;
+	temp->next = NULL;
+	temp->prev = p;
+}
+
+void printList(node* list)
+{
+	node* temp = list;
+	while (temp != NULL)
+	{
+		if (temp->data != -1337)
+		{
+			cout << temp->data << "";
+		}
+		//cout << "\t";
+		temp = temp->next;
+	}
+	//if (temp != NULL && temp->data != NULL)
+	cout << endl;
+}
+
 int main(int argc, char* argv[])
 {
+	stack<char> myStack;
 	//Set input
-	std::string filename = "abc.html";
-	//string filename = argv[1];
+	//std::string filename = "abc.html";
+	int linecount = 1;
+	string filename = argv[1];
 	/*filename.erase(0, 6);
 
 	std::string s = filename;
@@ -38,17 +101,206 @@ int main(int argc, char* argv[])
 	ifstream instream(filename);//Obtain Input Stream
 	//ifstream opstream(s);// Obtain Operation Stream
 
-	string faf;
+	node *head = NULL;
+	//head = newNode(NULL);
+	string steiner;// reading string line
+	string element;
+	char stackerino[256]; // stack for storing characters
+	int charpoint = 1; // index of the stack
+
+	/*while (!instream.eof())
+	{
+		getline(instream, steiner);
+		istringstream some(steiner);
+		while (!some.eof()) {// loop for each element
+			linecount++;
+			getstring(some, element);
+			cout << element << endl;
+		}
+		//cout << steiner << endl;
+	}*/
 
 	while (!instream.eof())
 	{
-		getline(instream, faf);
-		cout << "Line: " << faf << endl;
+		//cout << "Line Number: " << linecount<<endl;
+		getline(instream, steiner);
+		//cout << steiner;
+		
+		istringstream some(steiner);
+		while (!some.eof())
+		{
+			//some.get();
+			element = some.get();
+
+			if (element == "<")
+			{
+				myStack.push('<');
+			}
+			if (element == ">")
+			{
+				if (myStack.size() != 0 && myStack.top() == '<')
+				{
+					//cout << "pop <" << endl;
+					myStack.pop();
+				}
+				else
+				{
+					cout << "Error in line " << linecount << ", top of stack=";
+					if (myStack.size() != 0)
+					{
+						cout << myStack.top();
+					}
+
+					cout << " current=" << ">" << endl;
+					//system("pause");
+					return 0;
+				}
+				//myStack.push('>');
+			}
+
+			if (element == "{")
+			{
+				myStack.push('{');
+			}
+			if (element == "}")
+			{
+				if (myStack.size() != 0 && myStack.top() == '{')
+				{
+					//cout << "pop <" << endl;
+					myStack.pop();
+				}
+				else
+				{
+					cout << "Error in line " << linecount << ", top of stack=";
+					if (myStack.size() != 0)
+					{
+						cout << myStack.top();
+					}
+
+					cout << " current=" << "}" << endl;
+					//system("pause");
+					return 0;
+				}
+				//myStack.push('>');
+			}
+
+			if (element == "(")
+			{
+				myStack.push('(');
+			}
+			if (element == ")")
+			{
+				if (myStack.size() != 0 && myStack.top() == '(')
+				{
+					//cout << "pop <" << endl;
+					myStack.pop();
+				}
+				else
+				{
+					cout << "Error in line " << linecount << ", top of stack=";
+					if (myStack.size() != 0)
+					{
+						cout << myStack.top();
+					}
+
+					cout << " current=" << ")" << endl;
+					//system("pause");
+					return 0;
+				}
+				//myStack.push('>');
+			}
+
+			if (element == "[")
+			{
+				myStack.push('[');
+			}
+			if (element == "]")
+			{
+				if (myStack.size() != 0 && myStack.top() == '[')
+				{
+					//cout << "pop [" << endl;
+					myStack.pop();
+				}
+				else
+				{
+					cout << "Error in line " << linecount << ", top of stack=";
+					if (myStack.size() != 0)
+					{
+						cout << myStack.top();
+					}
+					
+					cout << " current=" << "]" << endl;
+					//system("pause");
+					return 0;
+				}
+				//myStack.push(']');
+			}
+			
+
+			if (element == "\"")
+			{
+				
+				if (myStack.size() != 0 && myStack.top() == '"')
+				{
+					//cout << "pop <" << endl;
+					myStack.pop();
+				}
+				else
+				{
+					myStack.push('"');
+				}
+
+			}
+
+			if (element == "'")
+			{
+
+				if (myStack.size() != 0 && myStack.top() == '\'')
+				{
+					//cout << "pop <" << endl;
+					myStack.pop();
+				}
+				else
+				{
+					myStack.push('\'');
+				}
+
+			}
+
+			//cout << "Element: " << element<< endl;
+		}
+		linecount++;
 	}
 
 	//cout << "Filename: " << filename << endl;
 	//cout << "First Line: " << faf << endl;
+	
+	/*cout << "Number of elements on hte stack " << myStack.size() << endl;
+	while (!myStack.empty())
+	{
+		cout << "popping " << myStack.top() << endl;
+		myStack.pop();
+	}
+	cout << "Number of elements on hte stack " << myStack.size() << endl;*/
 
-	system("pause");
+
+	if (myStack.size() > 0)
+	{
+		cout << "Error in line " << linecount << ", top of stack=";
+		if (myStack.size() != 0)
+		{
+			cout << myStack.top();
+		}
+
+		cout << " current=" << "$" << endl;
+		//system("pause");
+		return 0;
+	}
+
+	cout << "ok" << endl;
+	
+
+
+	//system("pause");
 	return 0;
 }
