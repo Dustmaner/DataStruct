37756017cd537abd46127928c8f7ace7bdec981b
Dustmaner <erick.soy@hotmail.com>
2016-03-24 21:18:56 -0500

    Update tagminate.cpp

--- tagminate.cpp
+++ tagminate.cpp
@@ -15,29 +15,39 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	//Set input
-	std::string filename = "input=textcase.txt;operations=commands.txt";
+	std::string filename = "abc.html";
 	//string filename = argv[1];
-	filename.erase(0, 6);
+	/*filename.erase(0, 6);
 
 	std::string s = filename;
 	std::string delimiter = "=";
 
-	size_t pos = 0;// Get Operations.txt per node
+	size_t pos = 0;
 	std::string token;
 	while ((pos = s.find(delimiter)) != std::string::npos) {
 		token = s.substr(0, pos);
 		s.erase(0, pos + delimiter.length());
 	}
 
-	string operations = s;//obtain DigitsPerNode
+	string operations = s;
 
 	std::string f = filename;//get correct filename
 	std::string cutoff = ";";
-	std::string file = f.substr(0, f.find(cutoff)); //correct filename 
+	std::string file = f.substr(0, f.find(cutoff)); */
 
-	ifstream instream(file);//Obtain Input Stream
-	ifstream opstream(s);// Obtain Operation Stream
+	ifstream instream(filename);//Obtain Input Stream
+	//ifstream opstream(s);// Obtain Operation Stream
 
+	string faf;
+
+	while (!instream.eof())
+	{
+		getline(instream, faf);
+		cout << "Line: " << faf << endl;
+	}
+
+	//cout << "Filename: " << filename << endl;
+	//cout << "First Line: " << faf << endl;
 
 	system("pause");
 	return 0;
